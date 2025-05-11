d09d0e61cf202c81a8e6a1602679698115640a68
TamizhSK <tamizhsk1@gmail.com>
2025-05-11 14:40:16 +0530

    chore : changed the file and updates

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
     deviceSizes: [320, 420, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/avif', 'image/webp'], // Enable modern formats
-    domains: ['http://localhost:3000'], // Add your trusted domains here
+    domains: ['https://localhost:3000'], // Add your trusted domains here
   },
 
   // Compression for faster load times
